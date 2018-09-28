650c785dd72494dcc09c42a5e70954b3e8cfed7d
wgsnmm <wgsnmm@aliyun.com>
2018-09-28 15:37:43 +0800

    weibobo_ok

--- Ajax_study.py
+++ Ajax_study.py
@@ -1,10 +1,12 @@
 from urllib.parse import urlencode
+
 import requests
+from pyquery import PyQuery as pq
 
 base_url = 'https://m.weibo.cn/api/container/getIndex?'
 
 headers = {
-    'Host':'mweibo.cn',
+    'Host':'m.weibo.cn',
     'Referer': 'https://m.weibo.cn/u/2830678474',
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.81 Safari/537.36',
     'X-Requested-With': 'XMLHttpRequest'
@@ -14,30 +16,31 @@ def get_page(page):
         'type':'uid',
         'value':'2830678474',
         'containerid':'1076032830678474',
-        'page':'page'
+        'page':page
     }
     url = base_url + urlencode(params)
     try:
         response = requests.get(url,headers=headers)
         if response.status_code == 200:
+            print(response.status_code)
             return response.json()
     except requests.ConnectionError as e:
         print('Error', e.args)
 
-from pyquery import PyQuery as pq
 
 def parse_page(json):
     if json:
         items = json.get('data').get('cards')
         for item in items:
             item = item.get('mblog')
-            weibo = {}
-            weibo['id'] = item.get('id')
-            weibo['text'] = pq(item.get('text')).text()
-            weibo['attitudes'] = item.get('attitudes_count')
-            weibo['comments'] = item.get('comments_count')
-            weibo['reposts'] = item.get('reposts_count')
-            yield weibo
+            if item:
+                weibo = {}
+                weibo['id'] = item.get('id')
+                weibo['text'] = pq(item.get('text')).text()
+                weibo['attitudes'] = item.get('attitudes_count')
+                weibo['comments'] = item.get('comments_count')
+                weibo['reposts'] = item.get('reposts_count')
+                yield weibo
 if __name__=='__main__':
     for page in range(1,11):
         json = get_page(page)
